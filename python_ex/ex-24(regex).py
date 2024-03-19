69537ca121912d4eef3093bc7cae7a4c5085e199
Changsu Park <gd12091@gmail.com>
2024-03-19 13:55:36 +0900

    reformat

--- python_ex/ex-24(regex).py
+++ python_ex/ex-24(regex).py
@@ -3,8 +3,8 @@ pattern = r'ab+'
 test_string = 'abnormal'
 
 if re.match(pattern, test_string):
-    print("The string '{}' matches the pattern.".format(test_string))
+    print(f'The string "{test_string}" matches the pattern.')
 else:
-    print("The string '{}' does not match the pattern.".format(test_string))
+    print(f'The string "{test_string}" does not match the pattern.')
 
 #2019112163 박창수
